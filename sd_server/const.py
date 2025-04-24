2efc4e6006a333c5b2a954d40a1308efb5806c01
hello0zai <hello0zaizai@gmail.com>
2025-04-24 14:37:58 +0700

    Updated the constant variables.

--- sd_server/const.py
+++ sd_server/const.py
@@ -1,4 +1,15 @@
+# DEVELOPMENT_MODE = 0 is for local development.
+# DEVELOPMENT_MODE = 1 is for production.
+
+DEVELOPMENT_MODE = 1
+
 PROTOCOL = "https"
-HOST = 'ralvie.minervaiotstaging.com'
+
+# HOST = 'ralvie.minervaiotstaging.com'
+
+if DEVELOPMENT_MODE == 0:
+    HOST = 'ralvie.minervaiotstaging.com'
+else:
+    HOST = 'me.ralvie.ai'
 
 CACHE_KEY = "Sundial"
