ae2b6a0c64be1216664830b49179b8e40b8bf529
hello0zai <hello0zaizai@gmail.com>
2025-06-02 10:39:43 +0700

    Added STAGING constant variable

--- sd_server/const.py
+++ sd_server/const.py
@@ -2,6 +2,7 @@
 # DEVELOPMENT_MODE = 1 is for production.
 
 DEVELOPMENT_MODE = 1
+STAGING = 1
 
 PROTOCOL = "https"
 
@@ -10,6 +11,9 @@ PROTOCOL = "https"
 if DEVELOPMENT_MODE == 0:
     HOST = 'ralvie.minervaiotstaging.com'
 else:
-    HOST = 'me.ralvie.ai'
+    if STAGING == 1:
+        HOST = 'ralvie.minervaiotstaging.com'
+    else:
+        HOST = 'me.ralvie.ai'
 
 CACHE_KEY = "Sundial"
