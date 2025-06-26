4c7a0794563e0917d00cd2b9bb7e2dbf33e0a2eb
hello0zai <hello0zaizai@gmail.com>
2025-06-26 09:26:50 +0700

    Updated the environment variables.

--- sd_server/const.py
+++ sd_server/const.py
@@ -1,19 +1,23 @@
 # DEVELOPMENT_MODE = 0 is for local development.
 # DEVELOPMENT_MODE = 1 is for production.
 
-DEVELOPMENT_MODE = 1
-STAGING = 1
+DEVELOPMENT_MODE = 0
+STAGING = 0
 
 PROTOCOL = "https"
 
-# HOST = 'ralvie.minervaiotstaging.com'
+HOST_DEV = 'ralvie.minervaiotstaging.com'
+HOST_PRO = 'me.ralvie.ai'
 
 if DEVELOPMENT_MODE == 0:
-    HOST = 'ralvie.minervaiotstaging.com'
+    if STAGING == 1:
+        HOST = HOST_DEV
+    else:
+        HOST = HOST_PRO
 else:
     if STAGING == 1:
-        HOST = 'ralvie.minervaiotstaging.com'
+        HOST = HOST_DEV
     else:
-        HOST = 'me.ralvie.ai'
+        HOST = HOST_PRO
 
 CACHE_KEY = "Sundial"
