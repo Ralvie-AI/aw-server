fc8477f32983b371da607ec52cbbdc3baf21b8e4
hello0zai <hello0zaizai@gmail.com>
2025-07-03 15:50:10 +0700

    Set the status to 2.

--- sd_server/api.py
+++ sd_server/api.py
@@ -429,9 +429,10 @@ class ServerAPI:
                         return {"status": "success"}
                     elif response_data.get("code") == 'RCE0219':
                         event_ids = [obj['event_id'] for obj in events]
-                        self.db.update_server_sync_status(list_of_ids=event_ids, new_status=1)
-                        stop_module('sd-watcher-afk')
-                        stop_module('sd-watcher-window')
+                        self.db.update_server_sync_status(list_of_ids=event_ids, new_status=2)
+                        logger.info(f"Updated the events of mismatched mac address to 2.")
+                        # stop_module('sd-watcher-afk')
+                        # stop_module('sd-watcher-window')
                         return {"status": "success"}
                     else:
                         logger.error(f"Unexpected response code: {response_data.get('code')}")
