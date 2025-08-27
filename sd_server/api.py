842e722495d8b0e7cff23f6abc3b190b440ff6ac
hello0zai <hello0zaizai@gmail.com>
2025-08-27 16:04:14 +0700

    Add test status.

--- sd_server/api.py
+++ sd_server/api.py
@@ -1042,6 +1042,32 @@ class ServerAPI:
                     self.last_event[bucket_id] = merged
                     self.db[bucket_id].replace_last(merged)
                     return merged
+
+                    # test status
+                    # result = self.db[bucket_id].replace_last(merged)
+                    # logger.info(f"result type => {result} => {type(result)}")
+                    # if result != 1:
+                    #     logger.info(f"replace_last result = {result}")
+                    #     self.last_event[bucket_id] = merged
+                    #     # self.db[bucket_id].replace_last(merged)
+                    #     return merged
+                    # else:
+                    #     logger.debug("Inserting new heartbeat...")                       
+                    #     logger.info(f"heartbeat data {heartbeat}")
+                    #     logger.info(f"heartbeat data heartbeat type {type(heartbeat)}")
+                    #     heartbeat.id = None
+                    #     heartbeat.duration = 0
+                    #     logger.info(f"after heartbeat data {heartbeat}")
+                    #     # heartbeat = self.db[bucket_id].insert(heartbeat)
+                    #     heartbeat = self.db[bucket_id].insert(heartbeat)
+                    #     if not heartbeat:
+                    #         logger.warning("Failed to insert heartbeat")
+                    #     else:
+                    #         logger.debug(f"Inserted heartbeat with ID {heartbeat.id}")
+                    #     logger.info(f"heartbeat return data {heartbeat}")
+                    #     self.last_event[bucket_id] = heartbeat
+                    #     return heartbeat
+                    
                 else:
                     logger.debug(
                         "Received heartbeat after pulse window, inserting as new event. (bucket: {}) (app: {})".format(
