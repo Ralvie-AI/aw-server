9db5a676a2d55890754c51e39a09fb0af14afafa
chompu <chompu@zktecodev.com>
2025-12-11 14:35:11 +0700

    Update {email}-{company_id}-public.pem

--- sd_server/screen_shot.py
+++ sd_server/screen_shot.py
@@ -22,7 +22,7 @@ if sys.platform == "win32":
     PUBLIC_KEY = os.path.join(os.environ['LOCALAPPDATA'], "Sundial", "Sundial", "sd-server", "public.pem")
 elif sys.platform == "darwin":
     PUBLIC_KEY = os.path.join(os.path.expanduser("~"),
-                "Library", "Application Support", "Sundial", "sd-server", "public.pem")
+                "Library", "Application Support", "Sundial", "sd-server", '{email}-{company_id}-public.pem')
 
 blueprint = Blueprint("screenshot", __name__, url_prefix="/screenshot")
 
@@ -73,7 +73,8 @@ def screenshot():
     UUID = get_uuid_address()
 
     associated_data = f"image_format={image_format},user_id={user_id},company_id={company_id},UUID={UUID}".encode('utf-8')
-    encrypted_data_json = encrypt_image_to_json_gcm(file_location, associated_data, public_key_path=PUBLIC_KEY)
+    public_key_file = PUBLIC_KEY.format(email=creds.get('email'), company_id=company_id)
+    encrypted_data_json = encrypt_image_to_json_gcm(file_location, associated_data, public_key_path=public_key_file)
     file_path_without_ext, ext = os.path.splitext(file_location)
     json_file = f"{file_path_without_ext}.json"
 
