1580216c73a31467fc42c78fa460e954b996fca9
chompu <chompu@zktecodev.com>
2026-02-04 13:09:00 +0700

    Add handle long sleep issue

--- sd_server/api.py
+++ sd_server/api.py
@@ -37,6 +37,7 @@ from sd_transform import heartbeat_merge
 from sd_server.utils import get_uuid_address, send_to_gui, convert_datetime_string
 from sd_main.sd_desktop.monitor import  stop_process, get_running_process_id
 
+
 from .__about__ import __version__
 from .exceptions import NotFound
 
@@ -265,7 +266,9 @@ class ServerAPI:
 
         if "accept-language" in data:
             headers.update({"accept-language": data.get('accept-language')})
-            
+        
+        logger.info(f"data => {data}")
+        logger.info(f"json dumps data => {json.dumps(data)}")
         if 'timeout' in data:
             timeout = data.pop("timeout")
             return req.post(
@@ -1204,6 +1207,14 @@ class ServerAPI:
                     # self.last_event[bucket_id] = merged
                     # self.db[bucket_id].replace_last(merged)
                     # return merged
+                    def get_minutes(duration):    
+                        total_seconds = duration.total_seconds()
+                        # Convert seconds to minutes
+                        minutes = total_seconds / 60
+                        return round(minutes)
+                    
+                    if merged.get('data').get('status') == 'afk' and get_minutes(merged.get('duration')) >= 30:        
+                        merged['duration'] = timedelta(minutes=30)
                     result = self.db[bucket_id].replace_last(merged)
                     # logger.info(f"result type => {result}")
                     if result != 1:
@@ -1627,7 +1638,18 @@ class ScreenShotQueue(threading.Thread):
         if not self._try_connect():
             logger.info("Initial connection attempt failed. Will retry.")
 
+        last_tick = time.time()
         while not self.should_stop():
+            now = time.time()
+            gap = now - last_tick
+
+            if gap > 60 * 10:  # 10 minutes
+                logger.info(
+                    f"Long inactivity detected (gap={int(gap)}s), forcing reconnect"
+                )
+                self.connected = False
+
+            last_tick = now
             # Check internet connection and attempt to sync
             print("is_internet_connected()", is_internet_connected())
             if is_internet_connected():
