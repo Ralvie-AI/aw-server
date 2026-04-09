5d776f80c90e6c169a0aa2828f84599250eba030
hello0zai <hello0zaizai@gmail.com>
2026-04-09 13:03:27 +0700

    remove duplicate functions and unused code

--- sd_server/const.py
+++ sd_server/const.py
@@ -1,6 +1,6 @@
 import os 
 
-from sd_core.const import DEVELOPMENT_MODE, STAGING
+from sd_core.const import DEVELOPMENT_MODE, STAGING, CONFIG_SERVER
 
 PROTOCOL = "https"
 
@@ -28,8 +28,6 @@ else:
 SUCCESSFUL_SYNC_STATUS = "RCI0000" # store the events successful in server side
 REJECTED_SYNC_STATUS = "RCE0219" # server rejected these events
 NO_USER_FOUND = "RCE0039" # User does not exist
-
-CONFIG_SERVER = 0
 
 if CONFIG_SERVER == 1:
     import os
