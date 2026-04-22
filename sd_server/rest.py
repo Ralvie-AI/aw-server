e005da53a3328bde05d4354a2b8378897b48e747
hello0zai <hello0zaizai@gmail.com>
2026-04-22 10:00:46 +0700

    refactor: convert commonly used variables to constants

--- sd_server/rest.py
+++ sd_server/rest.py
@@ -10,6 +10,7 @@ from datetime import datetime, timedelta
 import iso8601
 import pytz
 import jwt
+from dateutil.parser import parse
 from flask_restx import Api, Resource, fields
 from flask import (
     Blueprint,
@@ -22,7 +23,7 @@ from flask import (
 
 from sd_core.launch_start import delete_launch_app, launch_app, set_autostart_registry
 from sd_core.util import authenticate, is_internet_connected, reset_user
-from sd_core.const import DEVELOPMENT_MODE
+from sd_core.const import DEVELOPMENT_MODE, CACHE_KEY
 from sd_core import schema, db_cache
 from sd_core.models import Event
 from sd_core.cache import *
@@ -232,8 +233,7 @@ class UserResource(Resource):
 
          @return a dictionary containing the user's details and a boolean indicating if the user was
         """
-        cache_key = "Sundial"
-        cached_credentials = cache_user_credentials("Sundial")
+        cached_credentials = cache_user_credentials(CACHE_KEY)
         # If internet connection is not connected to internet and try again.
         if not is_internet_connected():
             print("Please connect to internet and try again.")
@@ -340,8 +340,7 @@ class LoginResource(Resource):
          @return Response code and JSON
         """
         data = request.get_json()
-        cache_key = "Sundial"
-        cached_credentials = cache_user_credentials("Sundial")
+        cached_credentials = cache_user_credentials(CACHE_KEY)
         user_key = cached_credentials.get("user_key")
 
         # Returns a JSON object with the user_key data.
@@ -363,9 +362,7 @@ class LoginResource(Resource):
 
          @return 200 if user exist 401 if user does not exist
         """
-        data = request.get_json()
-        cache_key = "Sundial"
-        cached_credentials = cache_user_credentials("Sundial")
+        cached_credentials = cache_user_credentials(CACHE_KEY)
         # Returns the encrypted_db_key if the cached credentials are cached.
         if cached_credentials is not None:
             user_key = cached_credentials.get("encrypted_db_key")
@@ -388,7 +385,6 @@ class RalvieLoginResource(Resource):
 
          @return A JSON with the result of the authentication and user
         """
-        cache_key = "Sundial"
         refresh_token = ""
         # Check Internet Connectivity
         response_data = {}
@@ -419,7 +415,7 @@ class RalvieLoginResource(Resource):
         # Returns a JSON response with the user credentials.
         if auth_result.status_code == 200 and json.loads(auth_result.text)["code"] == 'UASI0011':
             # Retrieve Cached User Credentials
-            cached_credentials = cache_user_credentials("Sundial")
+            cached_credentials = cache_user_credentials(CACHE_KEY)
             token = json.loads(auth_result.text)["data"]["access_token"]
             # Get the User Key
             user_key = cached_credentials.get(
@@ -441,10 +437,10 @@ class RalvieLoginResource(Resource):
             # Generate JWT
             payload = {
                 "user": getpass.getuser(),
-                "email": cache_user_credentials("Sundial").get("email"),
-                "phone": cache_user_credentials("Sundial").get("phone"),
+                "email": cache_user_credentials(CACHE_KEY).get("email"),
+                "phone": cache_user_credentials(CACHE_KEY).get("phone"),
             }
-            encoded_jwt = jwt.encode(payload, cache_user_credentials("Sundial").get("user_key"),
+            encoded_jwt = jwt.encode(payload, cache_user_credentials(CACHE_KEY).get("user_key"),
                                      algorithm="HS256")
 
             # Response
@@ -796,7 +792,7 @@ class HeartbeatResource(Resource):
 
         # Proceed with heartbeat processing
         heartbeat = Event(**heartbeat_data)
-        cached_credentials = cache_user_credentials("Sundial")
+        cached_credentials = cache_user_credentials(CACHE_KEY)
 
         if cached_credentials is None:
             return {"message": "No cached credentials."}, 400
@@ -1273,8 +1269,7 @@ class User(Resource):
 
          @return JSON with firstname lastname and email or False if not
         """
-        cache_key = "Sundial"
-        cached_credentials = cache_user_credentials("Sundial")
+        cached_credentials = cache_user_credentials(CACHE_KEY)
         user_key = cached_credentials.get(
             "encrypted_db_key") if cached_credentials else None
         # Returns a JSON response with the user s credentials.
