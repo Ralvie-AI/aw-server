39350eb42e8966aa23b3e45e8f78695a22e5e2db
hello0zai <hello0zaizai@gmail.com>
2026-06-10 08:53:49 +0700

    refactor: comment unused code for future removal and remove unused code

--- sd_server/rest.py
+++ sd_server/rest.py
@@ -24,7 +24,7 @@ from flask import (
 
 from sd_core.launch_start import delete_launch_app, launch_app, set_autostart_registry
 from sd_core.util import authenticate, is_internet_connected, reset_user
-from sd_core.const import CACHE_KEY, SETTINGS_CACHE_KEY, LOGGING_VERBOSE
+from sd_core.const import SETTINGS_CACHE_KEY, LOGGING_VERBOSE
 from sd_core import schema, db_cache
 from sd_core.models import Event
 from sd_core.cache import credentials
@@ -32,10 +32,10 @@ from sd_query.exceptions import QueryException
 from . import logger
 from .api import ServerAPI
 from .exceptions import BadRequest, Unauthorized
-from sd_qt.manager import Manager
+# from sd_qt.manager import Manager
 
 application_cache_key = "application_cache"
-manager = Manager()
+# manager = Manager()
 
 
 def get_potential_location_and_zone(minutes_difference):
@@ -1133,101 +1133,101 @@ class LogResource(Resource):
         return current_app.api.get_log(), 200
 
 
-@api.route('/0/start/')
-class StartModule(Resource):
-    @api.doc(security="Bearer")
-    @api.doc(params={"module": "Module Name", })
-    def get(self):
-        """
-         Start modules on the server. This will return a message to the client indicating that the module has started.
+# @api.route('/0/start/')
+# class StartModule(Resource):
+#     @api.doc(security="Bearer")
+#     @api.doc(params={"module": "Module Name", })
+#     def get(self):
+#         """
+#          Start modules on the server. This will return a message to the client indicating that the module has started.
 
 
-         @return JSON with the message that was sent to the client
-        """
-        module_name = request.args.get("module")
-        message = manager.start_modules(module_name)
-        return jsonify({"message": message})
+#          @return JSON with the message that was sent to the client
+#         """
+#         module_name = request.args.get("module")
+#         message = manager.start_modules(module_name)
+#         return jsonify({"message": message})
 
 
-@api.route('/0/stop/')
-class StopModule(Resource):
-    @api.doc(security="Bearer")
-    @api.doc(params={"module": "Module Name", })
-    def get(self):
-        """
-         Stop a module by name. This is a GET request to / v1 / modules / : id
+# @api.route('/0/stop/')
+# class StopModule(Resource):
+#     @api.doc(security="Bearer")
+#     @api.doc(params={"module": "Module Name", })
+#     def get(self):
+#         """
+#          Stop a module by name. This is a GET request to / v1 / modules / : id
 
 
-         @return JSON with message to
-        """
-        module_name = request.args.get("module")
-        message = manager.stop_modules(module_name)
-        return jsonify({"message": message})
+#          @return JSON with message to
+#         """
+#         module_name = request.args.get("module")
+#         message = manager.stop_modules(module_name)
+#         return jsonify({"message": message})
 
 
-@api.route('/0/status')
-class Status(Resource):
-    @api.doc(security="Bearer")
-    def get(self):
-        """
-         Get list of modules. This is a GET request to / modules. The response is a JSON object with a list of modules.
+# @api.route('/0/status')
+# class Status(Resource):
+#     @api.doc(security="Bearer")
+#     def get(self):
+#         """
+#          Get list of modules. This is a GET request to / modules. The response is a JSON object with a list of modules.
 
 
-         @return a JSON object with a list of modules in the
-        """
-        modules = manager.status()
+#          @return a JSON object with a list of modules in the
+#         """
+#         modules = manager.status()
 
-        return jsonify(modules)
+#         return jsonify(modules)
 
 
-@api.route('/0/idletime')
-class Idletime(Resource):
-    @api.doc(security="Bearer")
-    def get(self):
-        """
-        Manage the idle time state by starting or stopping the 'sd-watcher-afk' module.
+# @api.route('/0/idletime')
+# class Idletime(Resource):
+#     @api.doc(security="Bearer")
+#     def get(self):
+#         """
+#         Manage the idle time state by starting or stopping the 'sd-watcher-afk' module.
 
-        The 'status' query parameter controls whether the module is started or stopped:
-        - If 'status' is 'start', the module is started.
-        - If 'status' is 'stop', the module is stopped.
+#         The 'status' query parameter controls whether the module is started or stopped:
+#         - If 'status' is 'start', the module is started.
+#         - If 'status' is 'stop', the module is stopped.
 
-        @return a JSON object with a message indicating the new state.
-        """
+#         @return a JSON object with a message indicating the new state.
+#         """
 
-        try:
-            module = manager.module_status("sd-watcher-afk")
-            status = request.args.get("status")
+#         try:
+#             module = manager.module_status("sd-watcher-afk")
+#             status = request.args.get("status")
 
-            if module is None or "is_alive" not in module:
-                return {"message": "Module status could not be retrieved"}, 500
-            state = False
-            # Check the status argument and start/stop the module accordingly
-            if status == "start":
-                if not module["is_alive"]:
-                    manager.start("sd-watcher-afk")
-                    message = "Idle time has started"
-                    state = True
-                else:
-                    message = "Idle time is already running"
-                    state = True
-            elif status == "stop":
-                if module["is_alive"]:
-                    manager.stop("sd-watcher-afk")
-                    message = "Idle time has stopped"
-                    state = False
-                else:
-                    message = "Idle time is already stopped"
-                    state = False
-            else:
-                return {"message": "Invalid status parameter. Use 'start' or 'stop'."}, 400
+#             if module is None or "is_alive" not in module:
+#                 return {"message": "Module status could not be retrieved"}, 500
+#             state = False
+#             # Check the status argument and start/stop the module accordingly
+#             if status == "start":
+#                 if not module["is_alive"]:
+#                     manager.start("sd-watcher-afk")
+#                     message = "Idle time has started"
+#                     state = True
+#                 else:
+#                     message = "Idle time is already running"
+#                     state = True
+#             elif status == "stop":
+#                 if module["is_alive"]:
+#                     manager.stop("sd-watcher-afk")
+#                     message = "Idle time has stopped"
+#                     state = False
+#                 else:
+#                     message = "Idle time is already stopped"
+#                     state = False
+#             else:
+#                 return {"message": "Invalid status parameter. Use 'start' or 'stop'."}, 400
 
-            # Save the new idle time state in the settings
-            current_app.api.save_settings("idle_time", state)
-            return {"message": message}, 200
+#             # Save the new idle time state in the settings
+#             current_app.api.save_settings("idle_time", state)
+#             return {"message": message}, 200
 
-        except Exception as e:
-            logger.error(f"Error handling idle time: {str(e)}")
-            return {"message": "An error occurred while managing idle time."}, 500
+#         except Exception as e:
+#             logger.error(f"Error handling idle time: {str(e)}")
+#             return {"message": "An error occurred while managing idle time."}, 500
 
 
 
