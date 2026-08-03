0e31a06babf96c416996c443b4f49a20a350ebba
hello0zai <hello0zaizai@gmail.com>
2026-08-03 08:24:40 +0700

    feat: add create_user, check_mail, and check_password functions

--- sd_server/api.py
+++ sd_server/api.py
@@ -445,17 +445,6 @@ class ServerAPI:
         """
         return self.db.init_db()
 
-    def create_user(self, user:Dict[str, Any]):
-        """
-         Create a user on behalf of the authenticated user. This is a POST request to the ` ` / web / user ` ` endpoint.
-
-         @param user - A dictionary containing the information to create the user on behalf of.
-
-         @return The response from the server that was received as part of the request
-        """
-        endpoint = f"/web/user"
-        return self._post(endpoint , user)
-
     def authorize(self, user:Dict[str, Any]):
         """
          Authorize a user. This is a POST request to the ` / web / user / authorize ` endpoint.
@@ -1399,6 +1388,15 @@ class ServerAPI:
             return json.loads(events_json)
         else: return None
 
+    def create_user(self, email, password):
+        self.db.create_user(email, password)
+
+    def check_email(self, email):
+        return self.db.check_email(email)
+
+    def check_password(self, email, password):
+        return self.db.check_password(email, password)
+
 def datetime_serializer(obj):
     """
      Serialize datetime to ISO format. This is used to ensure that dates are converted to ISO format before saving to the database.
@@ -1698,24 +1696,6 @@ class ScreenShotQueue(threading.Thread):
                                         "--screenshot_id", str(record.id),                                        
                                     ]
                                 start_exe(command_list, timeout_sec=50)
-                                # ocr_result = self.orc.run_ocr(img_path=screenshot_file)
-                                # logger.info(f'result => {ocr_result}')
-                                # logger.info(f'result type=> {type(ocr_result)}')
-                                # self.server.db.update_ocr_text(record.id, ocr_result)
-                                # time.sleep(OCR_SLEEP_TIME)
-
-                                # screenshot = self.server.db.get_screenshot_by_id(record.id)
-
-                                # if LOGGING_VERBOSE == 1:                                    
-                                #     logger.info(f"after sleep {OCR_SLEEP_TIME} screenshot.ocr_text => {screenshot.ocr_text}")
-
-                                # logger.info(f"after sleep {OCR_SLEEP_TIME} screenshot.ocr_text length => {len(screenshot.ocr_text)}")
-
-                                # if screenshot.ocr_text is None:
-                                #     break
-
-                                # record = screenshot
-
                                 break
 
                             if LOGGING_VERBOSE == 1:
