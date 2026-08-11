d8b4c3dc9cb52f13bb92d126d66476229d211aba
hello0zai <hello0zaizai@gmail.com>
2026-08-11 10:53:45 +0700

    fix: use app instead of application in application_name and update Visual Studio Code to Code to be consistent with Mac app

--- sd_server/api.py
+++ sd_server/api.py
@@ -540,7 +540,9 @@ class ServerAPI:
                 local_zone = str(get_localzone()) 
                 for data in events:                    
                     data["clientTimeZone"] = local_zone
-                    data["sundial_version"] = RELEASE_VERSION 
+                    data["sundial_version"] = RELEASE_VERSION
+                    if data.get("app") == "Visual Studio Code":
+                        data["application_name"] = "Code"
                     
                 payload = {"userId": userId, "companyId": companyId, "events": events, "timeout": 60}
 
@@ -645,10 +647,9 @@ class ServerAPI:
         return dt.strftime("%Y-%m-%dT%H:%M:%SZ")
 
     def is_empty(self, record):
-        if record.event.application_name is None or record.event.application_name.strip() == "" or not len(record.event.application_name) > 2 :
-            return record.event.app
-        else:
-            return record.event.application_name
+        if record.event.app == "Visual Studio Code":
+            return "Code"
+        return record.event.app
 
     def get_local_capture_at(self, local_time_zone, record):
         if record.local_capture_at:
